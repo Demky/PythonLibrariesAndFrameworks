7140a62a061b2fd60a57409d4353d7affc3573e2
Demky <37147299+Demky@users.noreply.github.com>
2018-08-02 11:15:16 +0200

    Create py_unaccent.py

---
+++ py_unaccent.py
@@ -0,0 +1,8 @@
+import unicodedata
+
+def strip_accents(s):
+    return ''.join(c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn')
+
+
+# How to use :
+print(strip_accents('mystring : é è à â ê i o ô'))
